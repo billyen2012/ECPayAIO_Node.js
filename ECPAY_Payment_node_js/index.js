eecb297703e3fca0a2ce6bf40947ccd48dccb1e6
Bill Yen <billyen2012@gmail.com>
2025-06-26 19:42:04 +0800

    feat: add builder and optimize project structure

--- ECPAY_Payment_node_js/index.js
+++ ECPAY_Payment_node_js/index.js
@@ -1,6 +1,6 @@
 /**
  * Created by ying.wu on 2017/6/29.
  */
-'use strict';
+"use strict";
 
-module.exports = require('./lib/ecpay_payment.js');
+module.exports = require("./lib/ecpay_payment.js");
